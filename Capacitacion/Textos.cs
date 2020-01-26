ff027231447056b2ffa455385ebfb4860e034d32
Angel Albarracin <angel.albarracin.cv@gmail.com>
2020-01-26 12:45:45 -0300

    Cambio nombre de la app

---
+++ Capacitacion/Textos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Capacitacion
+{
+    internal class Textos
+    {
+        public static string AppName = AppInfo.Name;
+    }
+}
